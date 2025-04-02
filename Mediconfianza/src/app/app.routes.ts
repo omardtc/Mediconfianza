6f0e1ebfb226ea31bbb734ddb5443a8034f864c1
Omar Del Toro <19033@portalcedi.edu.mx>
2025-04-02 17:29:29 -0600

    First commit

---
+++ Mediconfianza/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./signup/signup.page').then( m => m.SignupPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'password',
+    loadComponent: () => import('./password/password.page').then( m => m.PasswordPage)
+  },
+];
