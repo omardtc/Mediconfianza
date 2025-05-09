c6b41cf30b5bdc2d5ae405fb70c210bf4ee5fba0
SantiagoLpzz <22014@portalcedi.edu.mx>
2025-05-09 08:52:14 -0600

    pacienteRegisterAvancex2

--- Mediconfianza/src/app/user-register/user-register.page.ts
+++ Mediconfianza/src/app/user-register/user-register.page.ts
@@ -6,7 +6,7 @@ import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { PacienteService } from '../paciente.service';
+import { Paciente, PacienteService } from '../paciente.service';
 
 @Component({
   selector: 'app-user-register',
@@ -23,7 +23,6 @@ export class UserRegisterPage implements OnInit {
   apellido = new FormControl('',[Validators.required,Validators.minLength(1)]);
   estado = new FormControl('',[Validators.required,Validators.minLength(1)]);
   fecha = new FormControl('',[Validators.required,Validators.minLength(1)]);
-  id = new FormControl('',[Validators.required,Validators.minLength(1)]);
   nombre = new FormControl('',[Validators.required,Validators.minLength(1)]);
   sexo = new FormControl('',[Validators.required,Validators.minLength(1)]);
   telefono = new FormControl('',[Validators.required,Validators.minLength(1)]);
@@ -33,11 +32,15 @@ export class UserRegisterPage implements OnInit {
   constructor(
     private authService: AuthService, 
     private alertController: AlertController, 
-    private router: Router
+    private router: Router,
+    private pS: PacienteService,
   ) { }
 
   ngOnInit() {
   }
+
+  userID: string = '';
+  userE: string | null = null;
 
   async onSubmit() {
     try {
@@ -57,6 +60,60 @@ export class UserRegisterPage implements OnInit {
       });
       await alert.present();
     }
+    if(this.validate()){
+      try {
+        const nPaciente: Paciente = {
+          apellido: this.apellido.value!,
+          estado: this.estado.value!,
+          fecha: this.fecha.value!,
+          id: this.userID!,
+          nombre: this.nombre.value!,
+          sexo: this.sexo.value!,
+          telefono: this.telefono.value!,
+          password: this.password!,
+          mail: this.email!,
+        };
+        await this.pS.addPaciente(nPaciente);
+        const alerta = await this.alertController.create({
+          header: 'SUCCESS',
+          message: 'hola',
+          buttons: ['adios'],
+        });
+        alerta.present();
+      } catch (error) {
+        const alerta = await this.alertController.create({
+          header: 'FUNNY FAIL',
+          message: 'Bruno joke, eh?',
+          buttons: ['WhYyYyy????'],
+        });
+        alerta.present();
+        this.reset();
+      }
+    } else {
+      console.log('Invalido');
+    }
+
+    if (this.validate())console.log('VALIDO');
+    else console.log('INVALIDO X2');
+  }
+
+  validate(){
+    console.log(this.apellido.value + ' ' + this.apellido.valid);
+    console.log(this.estado.value + ' ' + this.estado.valid);
+    console.log(this.fecha.value + ' ' + this.fecha.valid);
+    console.log(this.nombre.value + ' ' + this.nombre.valid);
+    console.log(this.sexo.value + ' ' + this.sexo.valid);
+    console.log(this.telefono.value + ' ' + this.telefono.valid);
+    return this.apellido.valid && this.estado.valid && this.fecha.valid && this.nombre.valid && this.sexo.valid && this.telefono.valid;
+  }
+
+  reset(){
+    console.log('----->  RESET  <-----');
+    this.apellido.reset;
+    this.estado.reset;
+    this.fecha.reset;
+    this.nombre.reset;
+    this.telefono.reset;
   }
 
   validateEmail(email: string): boolean {
